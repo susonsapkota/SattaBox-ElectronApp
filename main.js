b576442f45a90684f73251e992fed88337df523d
susonsapkota <susonsapkota@gmail.com>
2019-11-06 16:20:16 +0545

    Create function

--- main.js
+++ main.js
@@ -1,22 +1,81 @@
 const { app, BrowserWindow, ipcMain } = require("electron")
 const path = require('path');
 const url = require('url');
+var express = require('express');
+var japp = express();
+var fs = require("fs");
 
+var bodyParser = require('body-parser');
+
+
+
+
+var json = {
+	'data': []
+}
+var knex = require("knex")({
+	client: "sqlite3",
+	connection: {
+		filename: path.join(__dirname, 'database.sqlite')
+	},
+	useNullAsDefault: true
+});
+
+select=()=>{
+	knex.from('customers').select("*")
+	.then((rows) => {
+		json = {
+			'data': []
+		}
+		for (row of rows) {
+			json['data'].push(row);
+		}
+
+
+	}).catch((err) => { console.log(err); throw err })
+	.finally(() => {
+		// knex.destroy();
+	});
+
+}
+
+var server = japp.listen(8081, function () {
+	var host = server.address().address
+	var port = server.address().port
+	console.log("Example app listening at http://%s:%s", host, port);
+});
+
+
+japp.use(bodyParser.json());
+japp.get('/json', function (req, res) {
+	res.json(json);
+
+});
+japp.post('/form', function (req, res) {
+	data = req.body;
+	output = knex('customers').insert(data).then(()=>{select()});
+	res.send("Success");
+});
 app.on("ready", () => {
+	select();
 	let mainWindow = new BrowserWindow({
 		show: false, webPreferences: {
 			nodeIntegration: true
 		}
-	})
+	});
 	mainWindow.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
 		protocol: 'file',
 		slashes: true
 	}));
-	mainWindow.webContents.openDevTools();
+	
+	// mainWindow.setMenu(null)
+
 	mainWindow.maximize();
 	mainWindow.once("ready-to-show", () => {
+		mainWindow.webContents.openDevTools();
 		mainWindow.show();
+
 	});
 
 });
--- script.js
+++ script.js
@@ -1,60 +1,12 @@
 const path = require('path');
 const url = require('url');
 
-// customers=[]
-
-customers = [
-
-    {
-
-        "date": "1",
-        "name": "LanTest101",
-        "address": "123121212",
-        "telephone": "yLanTest101",
-        "old_stb": "M",
-        "new_stb": "10/16/1941",
-        "old_smart": "Caucasian/White",
-        "new_smart": "Caucasian/White",
-        "warranty": "Caucasian/White",
-        "remarks": "Bad",
-    },
-    {
-        "address": "kathmandu",
-        "date": "11/11/2019",
-        "name": "Suson",
-        "new_smart": "OXFFA126",
-        "new_stb": "OXFFA124",
-        "old_smart": "OXFFA125",
-        "old_stb": "OXFFA123",
-        "remarks": "Good",
-        "telephone": "+789 123456",
-        "warranty": "OXFFA127"
-    }
-
-]
-
-
-var knex = require("knex")({
-    client: "sqlite3",
-    connection: {
-        filename: path.join(__dirname, 'database.sqlite')
-    },
-    useNullAsDefault: true
-});
-
-knex('customers').select().then(function (data) {
-    //do something here
-    console.log(data);
-    data.forEach(function (singleRow) {
-        delete singleRow.id;
-        customers.push(singleRow);
-        console.log(singleRow);
-    });
-    console.log(customers)
+$(document).ready(function () {
     myTable = $('#dataTable').DataTable({
         destroy: true,
         dom: 'lifrtp',
-        "data": customers,
+        // "data": customers,
+        "ajax":"http://localhost:8081/json",
         "iDisplayLength": 25,
         "lengthChange": false,
         responsive: true,
@@ -80,53 +32,11 @@ knex('customers').select().then(function (data) {
             }
         ]
     });
-});
-
-
-$(document).ready(function () {
-
     $("#filterbox").keyup(function () {
         myTable.search(this.value).draw();
     });
 
 });
-
-
-function ConvertFormToJSON(form) {
-    var array = jQuery(form).serializeArray();
-    var json = {};
-
-    jQuery.each(array, function () {
-        json[this.name] = this.value || '';
-    });
-
-    return json;
-}
-
-$(function () {
-    $("form").on("submit", function (e) {
-        e.preventDefault();
-        console.log('clicked');
-        var form = this;
-        houses.push(ConvertFormToJSON(form))
-
-    }
-    );
-});
-
-
-
-function ConvertFormToJSON(form) {
-    var array = jQuery(form).serializeArray();
-    console.log(array);
-    var json = {};
-
-    jQuery.each(array, function () {
-        json[this.name] = this.value || '';
-    });
-
-    return json;
-}
 
 
 
